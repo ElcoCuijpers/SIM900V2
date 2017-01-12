31af97634873e9c78638998fba83b8b544774a16
ImecElcoCuijpers <Elco.Cuijpers.ext@imec-nl.nl>
2017-01-12 16:46:22 +0100

    Working with datalogging on showGPS.php

--- SIM900V2.ino
+++ SIM900V2.ino
@@ -2,7 +2,7 @@
 
 #include "SIM900.h"
 #include <SoftwareSerial.h>
-
+#include "gps.h"
 
 
 
@@ -22,6 +22,8 @@ MCMCU mobileControlledMCU;
 GSMI gsmInterface;
 GSMMCU GSMMicroControllerUnit;
 GPRSMCU GPRSMicroControllerUnit;
+
+GPSGSM gps;
 //To change pins for Software Serial, use the two lines in GSM.cpp.
 
 //GSM Shield for Arduino
@@ -64,7 +66,11 @@ void setup()
      Serial.println(mobileControlledMCU.getUnitId());
      }
       GPRSMicroControllerUnit.begin(&gsm,&mobileControlledMCU,&gsmInterface);
-    
+      delay(5000);
+     if (gps.attachGPS()){
+         Serial.println("status=GPSREADY");}
+      else{ Serial.println("status=ERROR");}
+      delay(5000);
    // gsm.SimpleWriteln("AT+CIFSR");
     //GPRSMicroControllerUnit.GPRSGetTime();
    // strcpy(timeSendString,"17/01/10,11:45:10+01");
@@ -72,6 +78,14 @@ void setup()
   
   
 };
+char sendstring[200];
+int stat;
+char lon[15];
+char lat[15];
+char alt[15];
+char timee[20];
+char vel[15];
+
 
 void loop()
 {
@@ -87,13 +101,48 @@ void loop()
       //strcpy(sendingString,"p1=0&p2=0&p10=0&p11=0&p13=0");
       
      // GPRSMicroControllerUnit.GPRSGetSetupParameter("time=12");
-      GPRSMicroControllerUnit.GPRSSendDataToSink("time=12");
-
+      strcpy(sendstring,"stat=");
+          stat=gps.getStat();
+          if(stat==1){
+               strcat(sendstring,"NOT FIXED");
+               Serial.println("NOT FIXED");}
+          else if(stat==0){
+               strcat(sendstring,"GPS OFF");
+               Serial.println("GPS OFF");}
+          else if(stat==2){
+               strcat(sendstring,"2D FIXED");
+               Serial.println("2D FIXED");}
+          else if(stat==3){
+               strcat(sendstring,"3D FIXED");
+               Serial.println("3D FIXED");}
+          delay(5000);
+          //Get data from GPS
+          gps.getParRMC(lon,lat,alt,timee,vel);
+        /*  Serial.println(lon);
+          Serial.println(lat);
+          Serial.println(alt);
+          Serial.println(time);
+          Serial.println(vel);*/
+          
+      strcat(sendstring,"&lon=");
+      strcat(sendstring,lon);
+      strcat(sendstring,"&lat=");
+      strcat(sendstring,lat);
+      strcat(sendstring,"&alt=");
+      strcat(sendstring,alt);
+      strcat(sendstring,"&time=");
+      strcat(sendstring,timee);
+      strcat(sendstring,"&vel=");
+      strcat(sendstring,vel);
+      
+      GPRSMicroControllerUnit.GPRSSendDataToSink(sendstring);
+      Serial.print("string:");
+      Serial.println(sendstring);
        
        //GSMMicroControllerUnit.incommingHandler();
        //GSMMicroControllerUnit.outgoingHandler();
        GPRSMicroControllerUnit.GPRSSendHandler();
-       delay(10000);
+       delay(20000);
  
      }
 };
